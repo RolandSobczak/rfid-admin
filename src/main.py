c42d204ad8ffbac25d276f85e864e0249dcede83
Roland Sobczak <rolandsobczak@gmail.com>
2024-08-09 21:02:38 +0200

    feat: fastapi added

---
+++ src/main.py
@@ -0,0 +1,16 @@
+def register_new_user():
+    pass
+
+
+def deploy_tenant():
+    pass
+
+
+def create_database():
+    pass
+
+
+def deploy_new_tenant():
+    create_database()
+    deploy_new_tenant()
+    register_new_user()
