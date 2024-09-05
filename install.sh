1af76a814896262db56917ed0b12af73fe37dd35
Roland Sobczak <rolandsobczak@gmail.com>
2024-09-05 16:49:12 +0200

    fix: hardcoded registry host replaced

--- install.sh
+++ install.sh
@@ -9,8 +9,6 @@ sudo mkdir -p "$BACKUP_DIR"
 shopt -s expand_aliases
 alias k=kubectl
 
-NODE='pop-os'
-
 k create namespace "$NAMESPACE"
 
 k create secret generic -n "$NAMESPACE" --from-env-file=env/postgres.env db
@@ -22,15 +20,17 @@ sed -e "s|%PG_VERSION%|${PG_VERSION}|g" \
     -e "s|%VOLUME_NAME%|${POSTGRES_VOLUME_NAME}|g" \
     -e "s|%STORAGE_SIZE%|${POSTGRES_STORAGE_SIZE}|g" \
     -e "s|%NAMESPACE%|${NAMESPACE}|g" \
-    -e "s|%NODE%|${NODE}|g" \
+    -e "s|%REGISTRY%|${REGISTRY}|g" \
     k8s/db.yml > "$BUILD_DIR/db.yml"
 
 sed -e "s|%RABBIT_VERSION%|${PG_VERSION}|g" \
     -e "s|%NAMESPACE%|${NAMESPACE}|g" \
+    -e "s|%REGISTRY%|${REGISTRY}|g" \
     k8s/rabbit.yml > "$BUILD_DIR/rabbit.yml"
 
 sed -e "s|%BACKUP_DIR%|${BACKUP_DIR}|g" \
     -e "s|%NAMESPACE%|${NAMESPACE}|g" \
+    -e "s|%REGISTRY%|${REGISTRY}|g" \
     k8s/auth.yml > "$BUILD_DIR/auth.yml"
 
 sed -e "s|%BACKUP_DIR%|${BACKUP_DIR}|g" \
@@ -38,22 +38,25 @@ sed -e "s|%BACKUP_DIR%|${BACKUP_DIR}|g" \
     -e "s|%STORAGE_CLASS%|${BACKUP_STORAGE_CLASS}|g" \
     -e "s|%VOLUME_NAME%|${BACKUP_VOLUME_NAME}|g" \
     -e "s|%STORAGE_SIZE%|${BACKUP_STORAGE_SIZE}|g" \
-    -e "s|%NODE%|${NODE}|g" \
     -e "s|%DOMAIN%|${DOMAIN}|g" \
     -e "s|%ROUTING%|${ROUTING}|g" \
+    -e "s|%REGISTRY%|${REGISTRY}|g" \
     k8s/admin.yml > "$BUILD_DIR/admin.yml"
 
 sed -e "s|%NAMESPACE%|${NAMESPACE}|g" \
-    -e "s|%NODE%|${NODE}|g" \
+    -e "s|%REGISTRY%|${REGISTRY}|g" \
     k8s/external.yml > "$BUILD_DIR/external.yml"
 
 sed -e "s|%NAMESPACE%|${NAMESPACE}|g" \
+    -e "s|%REGISTRY%|${REGISTRY}|g" \
     k8s/front.yml > "$BUILD_DIR/front.yml"
 
 sed -e "s|%NAMESPACE%|${NAMESPACE}|g" \
+    -e "s|%REGISTRY%|${REGISTRY}|g" \
     k8s/admin-front.yml > "$BUILD_DIR/admin-front.yml"
 
 sed -e "s|%NAMESPACE%|${NAMESPACE}|g" \
+    -e "s|%REGISTRY%|${REGISTRY}|g" \
     k8s/tenant-conf.yml > "$BUILD_DIR/tenant-conf.yml"
 
 k apply -f "$BUILD_DIR/db.yml"
